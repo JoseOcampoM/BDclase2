63fba06e8c821134063458ed2f2c0908be4a867a
Jose Ocampo <jocampo@uniguajira.edu.co>
2022-03-04 08:30:45 -0500

    Subir clase 2

--- ejercicio3.py
+++ ejercicio3.py
@@ -10,8 +10,26 @@ def listas():
     
     listaConElementos = [30, 2000000, "Jose Ocampo", "Estudiante", True, ["Magister", "Doctorado", "Especializacion", 20]]
     
-    print(listaConElementos)
+    #print(listaConElementos[1])
     
+    #Utiliizando el ciclo for
+    print("Mostrando elementos con ciclo for")
+    for i in range(len(listaConElementos)):
+        print(listaConElementos[i])
+        
+    
+    #Utilizando El ciclo while
+    print("Mostrando elementos con ciclo while" )
+    j=0
+    while j < len(listaConElementos):
+        print(listaConElementos[j])
+        j+=1
+        
+    print(listaConElementos[5][3])
+    print(listaConElementos[-1][3])
+    print(listaConElementos[0:3]) #Miuestra la posicion de la 0 hasta la 3 sin entrar la 3
+    print(listaConElementos[1:6:2])
+
 def main():
     listas()
     
